d26a90eb185936b4cbbadc5a2276d1ceef8a60c0
Greg Doud <gpdoud@gmail.com>
2019-02-08 14:36:29 -0500

    Initial commit

--- SalesByStateGroup/Program.cs
+++ SalesByStateGroup/Program.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace SalesByStateGroup {
     class Program {
+
         static void Main(string[] args) {
+
+            SalesByStateGroupLibrary.Class1 pgm = new SalesByStateGroupLibrary.Class1();
+
+            var total = pgm.Get("IN") + pgm.Get("NY") + pgm.Get("MN");
+            Console.WriteLine($"Sales for IN, NY, MN is {total}");
+
+            total = pgm.Get("KY") + pgm.Get("PA") + pgm.Get("MI") + pgm.Get("IL");
+            Console.WriteLine($"Sales for KY, PA, MI, IL is {total}");
         }
     }
 }
---
+++ SalesByStateGroupLibrary/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesByStateGroupLibrary
+{
+    public class Class1
+    {
+        private Dictionary<string, int> stateSales = new Dictionary<string, int>();
+
+        public int Get(string state) {
+            return stateSales[state];
+        }
+
+        public Class1() {
+            stateSales.Add("OH", 90416);
+            stateSales.Add("KY", 77408);
+            stateSales.Add("IN", 16588);
+            stateSales.Add("PA", 55043);
+            stateSales.Add("MI", 60099);
+            stateSales.Add("NY", 19830);
+            stateSales.Add("IL", 81154);
+            stateSales.Add("WI", 11679);
+            stateSales.Add("MN", 94207);
+            stateSales.Add("MS", 93521);
+        }
+    }
+}
